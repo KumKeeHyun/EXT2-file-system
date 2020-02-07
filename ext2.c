f612588b0fb4bf6b787cd86fb439410608fdda59
KumKeeHyun <kbzjung359@gmail.com>
2020-02-07 14:47:53 +0900

    reset ext2

--- ext2.c
+++ ext2.c
@@ -9,93 +9,7 @@ typedef struct
 
 int ext2_write(EXT2_NODE* file, unsigned long offset, unsigned long length, const char* buffer)
 {
-	BYTE	sector[MAX_SECTOR_SIZE];
-	DWORD	currentOffset, currentBlock, blockSeq = 0;
-	DWORD	blockNumber, sectorNumber, sectorOffset;
-	DWORD	readEnd;
-	DWORD	blockSize;
-	INODE node;
-	int i;
-
-	get_inode(file->fs, file->entry.inode, &node);
-	currentBlock = node.block[0];
-	readEnd = offset + length;
-
-	currentOffset = offset;
-
-	blockSize = MAX_BLOCK_SIZE;
-
-	i = 1;
-	while (offset > blockSize)
-	{
-
-		currentBlock = get_data_block_at_inode(file->fs, node, ++i);
-		blockSize += blockSize;
-		blockSeq++;
-	}
-
-	while (currentOffset < readEnd)
-	{
-		DWORD	copyLength;
-
-
-		blockNumber = currentOffset / MAX_BLOCK_SIZE;
-		if (currentBlock == 0)
-		{
-			if (expand_block(file->fs, file->entry.inode) == EXT2_ERROR)
-				return EXT2_ERROR;
-			process_meta_data_for_block_used(file->fs, file->entry.inode);
-			get_inode(file->fs, file->entry.inode, &node);
-			currentBlock = node.block[0];
-		}
-
-		if (blockSeq != blockNumber)
-		{
-			DWORD nextBlock;
-			blockSeq++;
-			++i;
-			nextBlock = get_data_block_at_inode(file->fs, node, i);
-			if (nextBlock == 0)
-			{
-				expand_block(file->fs, file->entry.inode);
-				get_inode(file->fs, file->entry.inode, &node);
-				process_meta_data_for_block_used(file->fs, file->entry.inode);
-
-				nextBlock = get_data_block_at_inode(file->fs, node, i);
-	
-				if (nextBlock == 0)
-				{
-					return EXT2_ERROR;
-				}
-			}
-			currentBlock = nextBlock;
-		}
-		sectorNumber = (currentOffset / (MAX_SECTOR_SIZE)) % (MAX_BLOCK_SIZE / MAX_SECTOR_SIZE);
-		sectorOffset = currentOffset % MAX_SECTOR_SIZE;
-
-		copyLength = MIN(MAX_SECTOR_SIZE - sectorOffset, readEnd - currentOffset);
-
-		if (copyLength != MAX_SECTOR_SIZE)
-		{
-			if (data_read(file->fs, 0, currentBlock, sector))
-				break;
-		}
-
-		memcpy(&sector[sectorOffset],
-			buffer,
-			copyLength);
-
-		if (data_write(file->fs, 0, currentBlock, sector))
-			break;
-
-		buffer += copyLength;
-		currentOffset += copyLength;
-	}
-
-	node.size = MAX(currentOffset, node.size);
-	set_inode_onto_inode_table(file->fs, file->entry.inode, &node);
-
-	return currentOffset - offset;
+	return 0;
 }
 
 UINT32 get_free_inode_number(EXT2_FILESYSTEM* fs);
@@ -204,7 +118,7 @@ int ext2_format(DISK_OPERATIONS* disk, UINT32 log_block_size)
 			disk->write_sector(disk, sector_num_per_group * gi + BOOT_SECTOR_BASE + i, sector);
 	}
     */
-   
+
 	PRINTF("max inode count                : %u\n", sb.max_inode_count);
 	PRINTF("total block count              : %u\n", sb.block_count);
 	PRINTF("byte size of inode structure   : %u\n", sb.inode_structure_size);
@@ -222,23 +136,6 @@ int fill_super_block(EXT2_SUPER_BLOCK * sb, SECTOR numberOfSectors, UINT32 bytes
 {
 	ZeroMemory(sb, sizeof(EXT2_SUPER_BLOCK));
 
-	sb->max_inode_count = NUMBER_OF_INODES;
-	sb->block_count = numberOfSectors;
-	sb->reserved_block_count = 0;
-	sb->free_block_count = numberOfSectors - (17 * NUMBER_OF_GROUPS) - 1;
-	sb->free_inode_count = NUMBER_OF_INODES - 10;
-	sb->first_data_block = 1;
-	sb->log_block_size = 0;
-	sb->log_fragmentation_size = 0;
-	sb->block_per_group = (numberOfSectors - 1) / NUMBER_OF_GROUPS;
-	sb->fragmentation_per_group = 0;
-	sb->inode_per_group = NUMBER_OF_INODES / NUMBER_OF_GROUPS;
-	sb->magic_signature = 0xEF53;
-	sb->errors = 0;
-	sb->first_non_reserved_inode = 11;
-	sb->inode_structure_size = 128;
-	sb->block_group_number = 0;
-	sb->first_data_block_each_group = 1 + 1 + 1 + 1 + 13;
 
 	return EXT2_SUCCESS;
 }
@@ -247,72 +144,12 @@ int fill_descriptor_block(EXT2_GROUP_DESCRIPTOR * gd, EXT2_SUPER_BLOCK * sb, SEC
 {
 	ZeroMemory(gd, sizeof(EXT2_GROUP_DESCRIPTOR));
 
-	gd->start_block_of_block_bitmap = 2;
-	gd->start_block_of_inode_bitmap = 3;
-	gd->start_block_of_inode_table = 4;
-	gd->free_blocks_count = (UINT16)(sb->free_block_count / NUMBER_OF_GROUPS + sb->free_block_count % NUMBER_OF_GROUPS);
-	gd->free_inodes_count = (UINT16)(((sb->free_inode_count) + 10) / NUMBER_OF_GROUPS - 10);
-	gd->directories_count = 0;
-
 	return EXT2_SUCCESS;
 }
 
 int create_root(DISK_OPERATIONS* disk, EXT2_SUPER_BLOCK * sb)
 {
-	BYTE   sector[MAX_SECTOR_SIZE];
-	SECTOR   rootSector = 0;
-	EXT2_DIR_ENTRY * entry;
-	EXT2_GROUP_DESCRIPTOR * gd;
-	EXT2_SUPER_BLOCK * sb_read;
-	QWORD sector_num_per_group = (disk->number_of_sectors - 1) / NUMBER_OF_GROUPS;
-	INODE * ip;
-	const int BOOT_SECTOR_BASE = 1;
-	int gi;
-
-	ZeroMemory(sector, MAX_SECTOR_SIZE);
-	entry = (EXT2_DIR_ENTRY*)sector;
-
-	memcpy(entry->name, VOLUME_LABLE, 11);
-	entry->name_len = strlen(VOLUME_LABLE);
-	entry->inode = 2;
-	entry++;
-	entry->name[0] = DIR_ENTRY_NO_MORE;
-	rootSector = 1 + sb->first_data_block_each_group;
-	disk->write_sector(disk, rootSector, sector);
-
-	sb_read = (EXT2_SUPER_BLOCK *)sector;
-	for (gi = 0; gi < NUMBER_OF_GROUPS; gi++)
-	{
-		disk->read_sector(disk, sector_num_per_group * gi + BOOT_SECTOR_BASE, sector);
-		sb_read->free_block_count--;
-
-		disk->write_sector(disk, sector_num_per_group * gi + BOOT_SECTOR_BASE, sector);
-	}
-	sb->free_block_count--;
-
-	gd = (EXT2_GROUP_DESCRIPTOR *)sector;
-	disk->read_sector(disk, BOOT_SECTOR_BASE + 1, sector);
-
-
-	gd->free_blocks_count--;
-	gd->directories_count = 1;
-
-	for (gi = 0; gi < NUMBER_OF_GROUPS; gi++)
-		disk->write_sector(disk, sector_num_per_group * gi + BOOT_SECTOR_BASE + 1, sector);
-
-	disk->read_sector(disk, BOOT_SECTOR_BASE + 2, sector);
-	sector[2] |= 0x02;
-	disk->write_sector(disk, BOOT_SECTOR_BASE + 2, sector);
-
-	ZeroMemory(sector, MAX_SECTOR_SIZE);
-	ip = (INODE *)sector;
-	ip++;
-	ip->mode = 0x1FF | 0x4000;
-	ip->size = 0;
-	ip->blocks = 1;
-	ip->block[0] = sb->first_data_block_each_group;
-	disk->write_sector(disk, BOOT_SECTOR_BASE + 4, sector);
-
+	
 	return EXT2_SUCCESS;
 }
 
@@ -486,35 +323,14 @@ int get_inode(EXT2_FILESYSTEM* fs, const UINT32 inode, INODE *inodeBuffer)
 
 int read_root_sector(EXT2_FILESYSTEM* fs, BYTE* sector)
 {
-	UINT32 inode = 2;
-	INODE inodeBuffer;
-	SECTOR rootBlock;
-	get_inode(fs, inode, &inodeBuffer);
-	rootBlock = get_data_block_at_inode(fs, inodeBuffer, 1);
-
-	return data_read(fs, 0, rootBlock, sector);
+	
+	return 0;
 }
 
 int ext2_create(EXT2_NODE* parent, char* entryName, EXT2_NODE* retEntry)
 {
-	if ((parent->fs->gd.free_inodes_count) == 0) return EXT2_ERROR;
-	UINT32 inode;
-	BYTE name[MAX_NAME_LENGTH] = { 0, };
-	BYTE sector[MAX_BLOCK_SIZE];
-	int result;
-
-	strcpy(name, entryName);
-	if (format_name(parent->fs, name) == EXT2_ERROR) return EXT2_ERROR;
-
-	ZeroMemory(retEntry, sizeof(EXT2_NODE));
-	memcpy(retEntry->entry.name, name, MAX_ENTRY_NAME_LENGTH);
-	retEntry->fs = parent->fs;
-	inode = parent->entry.inode;
-	if ((result = lookup_entry(parent->fs, inode, name, retEntry)) == EXT2_SUCCESS) return EXT2_ERROR;
-	else if (result == -2) return EXT2_ERROR;
-
-	if (insert_entry(inode, retEntry, 0) == EXT2_ERROR)return EXT2_ERROR;
-	return EXT2_SUCCESS;
+	
+    return EXT2_SUCCESS;
 }
 
 
@@ -524,31 +340,7 @@ int get_data_block_at_inode(EXT2_FILESYSTEM *fs, INODE inode, UINT32 number)
 
 int ext2_read_superblock(EXT2_FILESYSTEM* fs, EXT2_NODE* root)
 {
-	INT result;
-	BYTE sector[MAX_SECTOR_SIZE];
-
-	if (fs == NULL || fs->disk == NULL)
-	{
-		WARNING("DISK OPERATIONS : %p\nEXT2_FILESYSTEM : %p\n", fs, fs->disk);
-		return EXT2_ERROR;
-	}
-
-	meta_read(fs, 0, SUPER_BLOCK, sector);
-	memcpy(&fs->sb, sector, sizeof(EXT2_SUPER_BLOCK));
-	meta_read(fs, 0, GROUP_DES, sector);
-	memcpy(&fs->gd, sector, sizeof(EXT2_GROUP_DESCRIPTOR));
-
-	if (fs->sb.magic_signature != 0xEF53)
-		return EXT2_ERROR;
-
-	ZeroMemory(sector, sizeof(MAX_SECTOR_SIZE));
-	if (read_root_sector(fs, sector))
-		return EXT2_ERROR;
-
-	ZeroMemory(root, sizeof(EXT2_NODE));
-	memcpy(&root->entry, sector, sizeof(EXT2_DIR_ENTRY));
-	root->fs = fs;
-
+	
 	return EXT2_SUCCESS;
 }
 
@@ -562,72 +354,20 @@ int set_inode_onto_inode_table(EXT2_FILESYSTEM *fs, const UINT32 which_inode_num
 
 int ext2_lookup(EXT2_NODE* parent, const char* entryName, EXT2_NODE* retEntry)
 {
-	EXT2_DIR_ENTRY_LOCATION	begin;
-	BYTE	formattedName[MAX_NAME_LENGTH] = { 0, };
-
-	strcpy(formattedName, entryName);
-
-	if (format_name(parent->fs, formattedName))
-		return EXT2_ERROR;
-
-	return lookup_entry(parent->fs, parent->entry.inode, formattedName, retEntry);
+	
+	return 
 }
 
 int ext2_read_dir(EXT2_NODE* dir, EXT2_NODE_ADD adder, void* list)
 {
-	BYTE   sector[MAX_SECTOR_SIZE];
-	INODE* inodeBuffer;
-	UINT32 inode;
-	int i, result, num;
-
-	inodeBuffer = (INODE*)malloc(sizeof(INODE));
-
-	ZeroMemory(sector, MAX_SECTOR_SIZE);
-	ZeroMemory(inodeBuffer, sizeof(INODE));
-
-	result = get_inode(dir->fs, dir->entry.inode, inodeBuffer);
-	if (result == EXT2_ERROR)
-		return EXT2_ERROR;
-
-	for (i = 0; i < inodeBuffer->blocks; ++i)
-	{
-		num = get_data_block_at_inode(dir->fs, *inodeBuffer, i + 1);
-		data_read(dir->fs, 0, num, sector);
-
-		if (dir->entry.inode == 2)
-			read_dir_from_sector(dir->fs, sector + 32, adder, list);
-		else
-			read_dir_from_sector(dir->fs, sector, adder, list);
-	}
-
+	
 	return EXT2_SUCCESS;
 }
 
 int read_dir_from_sector(EXT2_FILESYSTEM* fs, BYTE* sector, EXT2_NODE_ADD adder, void* list)
 {
-	UINT i, max_entries_Per_Sector;
-	EXT2_DIR_ENTRY*   dir;
-	EXT2_NODE   node;
-
-	max_entries_Per_Sector = MAX_SECTOR_SIZE / sizeof(EXT2_DIR_ENTRY);
-	dir = (EXT2_DIR_ENTRY*)sector;
-
-	for (i = 0; i < max_entries_Per_Sector; i++)
-	{
-		if (dir->name[0] == DIR_ENTRY_FREE)
-			;
-		else if (dir->name[0] == DIR_ENTRY_NO_MORE)
-			break;
-		else
-		{
-			node.fs = fs;
-			node.entry = *dir;
-			adder(fs, list, &node);
-		}
-		dir++;
-	}
-
-	return (i == max_entries_Per_Sector ? 0 : -1);
+	
+	return 0;
 }
 
 char* my_strncpy(char* dest, const char* src, int length)
@@ -640,42 +380,6 @@ char* my_strncpy(char* dest, const char* src, int length)
 
 int ext2_mkdir(const EXT2_NODE* parent, const char* entryName, EXT2_NODE* retEntry)
 {
-	EXT2_NODE      dotNode, dotdotNode;
-	DWORD         firstCluster;
-	BYTE         name[MAX_NAME_LENGTH];
-	int            result;
-	int            i;
-
-	strcpy((char*)name, entryName);
-
-	if (format_name(parent->fs, (char*)name))
-		return EXT2_ERROR;
-
-	ZeroMemory(retEntry, sizeof(EXT2_NODE));
-	memcpy(retEntry->entry.name, name, MAX_ENTRY_NAME_LENGTH);
-	retEntry->entry.name_len = strlen((char*)retEntry->entry.name);
-	retEntry->fs = parent->fs;
-
-	result = insert_entry(parent->entry.inode, retEntry, FILE_TYPE_DIR);
-	if (result == EXT2_ERROR)
-		return EXT2_ERROR;
-
-	expand_block(parent->fs, retEntry->entry.inode);
-
-	ZeroMemory(&dotNode, sizeof(EXT2_NODE));
-	memset(dotNode.entry.name, 0x20, 11);
-	dotNode.entry.name[0] = '.';
-	dotNode.fs = retEntry->fs;
-	dotNode.entry.inode = retEntry->entry.inode;
-	insert_entry(retEntry->entry.inode, &dotNode, FILE_TYPE_DIR);
-
-	ZeroMemory(&dotdotNode, sizeof(EXT2_NODE));
-	memset(dotdotNode.entry.name, 0x20, 11);
-	dotdotNode.entry.name[0] = '.';
-	dotdotNode.entry.name[1] = '.';
-	dotdotNode.entry.inode = parent->entry.inode;
-	dotdotNode.fs = retEntry->fs;
-	insert_entry(retEntry->entry.inode, &dotdotNode, FILE_TYPE_DIR);
-
+	
 	return EXT2_SUCCESS;
 }
